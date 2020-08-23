022ebde195934cb5a6af38cf70598fa73ac8fa8c
ed-walpole-on-pc <43918391+ed-walpole-on-pc@users.noreply.github.com>
2020-08-23 03:51:37 +0100

    0.1.8
    Most home page content in place.

--- Website/scripts.js
+++ Website/scripts.js
@@ -6,58 +6,74 @@ var height = document.documentElement.scrollHeight - document.documentElement.cl
 var displayHeight = document.documentElement.clientHeight;
 var scrolled = (winScroll / height) * 100;
 document.getElementById("myBar").style.width = scrolled + "%";
-// document.getElementById("logotopright").style.opacity = winScroll/displayHeight;
-// console.log(winScroll);
 
 var welcome = document.getElementById('welcome').getBoundingClientRect();
-var aboutRwp = document.getElementById('about-rwp').getBoundingClientRect();
 var services = document.getElementById('services').getBoundingClientRect();
+var aboutRwp = document.getElementById('about-rwp').getBoundingClientRect();
+var contact = document.getElementById('contact').getBoundingClientRect();
+var careers = document.getElementById('careers').getBoundingClientRect();
+var company = document.getElementById('company').getBoundingClientRect();
+
 
 console.log(
   "welcome\nwelcome.y = " + welcome.y + "\nwelcome.top = " + welcome.top+ "\nwelcome.bottom = " + welcome.bottom )
   // "\naboutRwp\naboutRwp.y = " + aboutRwp.y + "\naboutRwp.top = " + aboutRwp.top+ "\naboutRwp.bottom = " + aboutRwp.bottom)
 
-// console.log(displayHeight)
 
 // Ternary Operator
 // ( condition ) ? run this code : run this code instead
 
-console.log("\nHeight = " + height);
-
 let welcomeArea = (welcome.top < 0) ? welcome.bottom : height - welcome.bottom;
-let aboutRwpArea = (aboutRwp.top < 0) ? aboutRwp.bottom : height - aboutRwp.bottom;
 let servicesArea = (services.top < 0) ? services.bottom : height - services.bottom;
+let aboutRwpArea = (aboutRwp.top < 0) ? aboutRwp.bottom : height - aboutRwp.bottom;
+let contactArea = (contact.top < 0) ? contact.bottom : height - contact.bottom;
+let careersArea = (careers.top < 0) ? careers.bottom : height - careers.bottom;
+let companyArea = (company.top < 0) ? company.bottom : height - company.bottom;
 
+
+
+console.log("\nHeight = " + height);
 console.log("\nwelcomeArea = " + welcomeArea);
 console.log("\naboutRwpArea = " + aboutRwpArea);
 
-let areas = [welcomeArea, aboutRwpArea, servicesArea];
-
+let areas = [welcomeArea, servicesArea, aboutRwpArea, contactArea, careersArea, companyArea];
 let section = indexOfMax(areas);
-
-// var sectionName;
 
 switch (section) {
   case 0: // welcome
-    // sectionName = "snwelcome";
-    // document.getElementById("snwelcome").style.textDecoration = "underline";
-    // document.getElementById("snabout-rwp").style.textDecoration = "none";
     document.getElementById("snwelcome").classList.add('current');
-    document.getElementById("snabout-rwp").classList.remove('current');
-    break;
-
-  case 1: // aboutRwp
-    // document.getElementById("snwelcome").style.textDecoration = "none";
-    // document.getElementById("snabout-rwp").style.textDecoration = "underline";
-    document.getElementById("snwelcome").classList.remove('current');
-    document.getElementById("snabout-rwp").classList.add('current');
     document.getElementById("snservices").classList.remove('current');
     break;
 
-  case 2: // services
-    document.getElementById("snabout-rwp").classList.remove('current');
+  case 1: // services
+    document.getElementById("snwelcome").classList.remove('current');
     document.getElementById("snservices").classList.add('current');
-    // document.getElementById("snservices").classList.remove('current');
+    document.getElementById("snabout-rwp").classList.remove('current');
+    break;
+
+  case 2: // aboutRwp
+    document.getElementById("snservices").classList.remove('current');
+    document.getElementById("snabout-rwp").classList.add('current');
+    document.getElementById("sncontact").classList.remove('current');
+    break;
+
+  case 3: // contact
+    document.getElementById("snabout-rwp").classList.remove('current');
+    document.getElementById("sncontact").classList.add('current');
+    document.getElementById("sncareers").classList.remove('current');
+    break;
+
+  case 4: // careers
+    document.getElementById("sncontact").classList.remove('current');
+    document.getElementById("sncareers").classList.add('current');
+    document.getElementById("sncompany").classList.remove('current');
+    break;
+
+  case 5: // company
+    document.getElementById("sncareers").classList.remove('current');
+    document.getElementById("sncompany").classList.add('current');
+    break;
+
 
   // default:
 }
