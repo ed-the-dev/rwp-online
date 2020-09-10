bd545e0746a47133d4eae8f56a729cf16c69dae2
ed-walpole-on-pc <43918391+ed-walpole-on-pc@users.noreply.github.com>
2020-09-10 04:10:23 +0100

    0.1.14
    iOS scroll snap implemented. Scroll progress updated to work. Writing my own animate on scroll.

---
+++ Website/myaos.js
@@ -0,0 +1,10 @@
+var elements = document.getElementsByClassName("show-on-scroll");
+
+
+
+
+console.log(elements);
+
+function aos(){
+  console.log("test")
+}
--- Website/scripts.js
+++ Website/scripts.js
@@ -1,90 +1,98 @@
-alert("Hello! I am an alert box!!");
+var elements = document.getElementsByClassName("show-on-scroll");
 
-window.onscroll = function() {progressIndicator()};
+// the html hasn't been read yet, so call this script after html?
 
-function progressIndicator() {
-var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-var displayHeight = document.documentElement.clientHeight;
-var scrolled = (winScroll / height) * 100;
-document.getElementById("myBar").style.width = scrolled + "%";
+function scrolling() {
+  var winScroll = document.getElementById("article").scrollTop;
+  var height = document.getElementById("article").scrollHeight - document.getElementById("article").clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
 
-var welcome = document.getElementById('welcome').getBoundingClientRect();
-var services = document.getElementById('services').getBoundingClientRect();
-var aboutRwp = document.getElementById('about-rwp').getBoundingClientRect();
-var contact = document.getElementById('contact').getBoundingClientRect();
-var careers = document.getElementById('careers').getBoundingClientRect();
-var company = document.getElementById('company').getBoundingClientRect();
+  var welcome = document.getElementById('welcome').getBoundingClientRect();
+  var services = document.getElementById('services').getBoundingClientRect();
+  var aboutRwp = document.getElementById('about-rwp').getBoundingClientRect();
+  var contact = document.getElementById('contact').getBoundingClientRect();
+  var careers = document.getElementById('careers').getBoundingClientRect();
+  var company = document.getElementById('company').getBoundingClientRect();
+
+  // Ternary Operator
+  // ( condition ) ? run this code : run this code instead
+
+  let welcomeArea = (welcome.top < 0) ? welcome.bottom : height - welcome.bottom;
+  let servicesArea = (services.top < 0) ? services.bottom : height - services.bottom;
+  let aboutRwpArea = (aboutRwp.top < 0) ? aboutRwp.bottom : height - aboutRwp.bottom;
+  let contactArea = (contact.top < 0) ? contact.bottom : height - contact.bottom;
+  let careersArea = (careers.top < 0) ? careers.bottom : height - careers.bottom;
+  let companyArea = (company.top < 0) ? company.bottom : height - company.bottom;
+
+  let areas = [welcomeArea, servicesArea, aboutRwpArea, contactArea, careersArea, companyArea];
+  let section = indexOfMax(areas);
+
+  switch (section) {
+    case 0: // welcome
+      document.getElementById("snwelcome").classList.add('current');
+      document.getElementById("snservices").classList.remove('current');
+      break;
+
+    case 1: // services
+      document.getElementById("snwelcome").classList.remove('current');
+      document.getElementById("snservices").classList.add('current');
+      document.getElementById("snabout-rwp").classList.remove('current');
+      break;
+
+    case 2: // aboutRwp
+      document.getElementById("snservices").classList.remove('current');
+      document.getElementById("snabout-rwp").classList.add('current');
+      document.getElementById("sncontact").classList.remove('current');
+      break;
+
+    case 3: // contact
+      document.getElementById("snabout-rwp").classList.remove('current');
+      document.getElementById("sncontact").classList.add('current');
+      document.getElementById("sncareers").classList.remove('current');
+      break;
+
+    case 4: // careers
+      document.getElementById("sncontact").classList.remove('current');
+      document.getElementById("sncareers").classList.add('current');
+      document.getElementById("sncompany").classList.remove('current');
+      break;
+
+    case 5: // company
+      document.getElementById("sncareers").classList.remove('current');
+      document.getElementById("sncompany").classList.add('current');
+      break;
+
+    }
+
+  // A N I M A T E  O N  S C R O L L //
+
+  // console.log(elements);
+
+  for (var i = 0; i < elements.length; i++) {
+    console.log(elements.item(i));
+  }
+
+  // check here that the element is being accessed.
 
 
-console.log(
-  "welcome\nwelcome.y = " + welcome.y + "\nwelcome.top = " + welcome.top+ "\nwelcome.bottom = " + welcome.bottom )
-  // "\naboutRwp\naboutRwp.y = " + aboutRwp.y + "\naboutRwp.top = " + aboutRwp.top+ "\naboutRwp.bottom = " + aboutRwp.bottom)
+  // .length;
 
-
-// Ternary Operator
-// ( condition ) ? run this code : run this code instead
-
-let welcomeArea = (welcome.top < 0) ? welcome.bottom : height - welcome.bottom;
-let servicesArea = (services.top < 0) ? services.bottom : height - services.bottom;
-let aboutRwpArea = (aboutRwp.top < 0) ? aboutRwp.bottom : height - aboutRwp.bottom;
-let contactArea = (contact.top < 0) ? contact.bottom : height - contact.bottom;
-let careersArea = (careers.top < 0) ? careers.bottom : height - careers.bottom;
-let companyArea = (company.top < 0) ? company.bottom : height - company.bottom;
+  // elements.forEach(
+  //   function (element) {
+  //   if (isElinVp(element)) {
+  //     element.classList.add('is-visible');
+  //   } else {
+  //     element.classList.remove('is-visible');
+  //   }
+  // });
 
 
 
-console.log("\nHeight = " + height);
-console.log("\nwelcomeArea = " + welcomeArea);
-console.log("\naboutRwpArea = " + aboutRwpArea);
-
-let areas = [welcomeArea, servicesArea, aboutRwpArea, contactArea, careersArea, companyArea];
-let section = indexOfMax(areas);
-
-switch (section) {
-  case 0: // welcome
-    document.getElementById("snwelcome").classList.add('current');
-    document.getElementById("snservices").classList.remove('current');
-    break;
-
-  case 1: // services
-    document.getElementById("snwelcome").classList.remove('current');
-    document.getElementById("snservices").classList.add('current');
-    document.getElementById("snabout-rwp").classList.remove('current');
-    break;
-
-  case 2: // aboutRwp
-    document.getElementById("snservices").classList.remove('current');
-    document.getElementById("snabout-rwp").classList.add('current');
-    document.getElementById("sncontact").classList.remove('current');
-    break;
-
-  case 3: // contact
-    document.getElementById("snabout-rwp").classList.remove('current');
-    document.getElementById("sncontact").classList.add('current');
-    document.getElementById("sncareers").classList.remove('current');
-    break;
-
-  case 4: // careers
-    document.getElementById("sncontact").classList.remove('current');
-    document.getElementById("sncareers").classList.add('current');
-    document.getElementById("sncompany").classList.remove('current');
-    break;
-
-  case 5: // company
-    document.getElementById("sncareers").classList.remove('current');
-    document.getElementById("sncompany").classList.add('current');
-    break;
-
-
-  // default:
 }
 
 
 
-
-
-}
 
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";}
@@ -116,3 +124,87 @@ function showMap() {
 function hideMap(){
   document.getElementById("withmap").classList.remove("show")
 }
+
+function isElinVp(element){
+
+}
+
+
+// window.onscroll = function() {progressIndicator()};
+
+// function progressIndicator() {
+// var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+// var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+// var displayHeight = document.documentElement.clientHeight;
+// var scrolled = (winScroll / height) * 100;
+// document.getElementById("myBar").style.width = scrolled + "%";
+//
+// var welcome = document.getElementById('welcome').getBoundingClientRect();
+// var services = document.getElementById('services').getBoundingClientRect();
+// var aboutRwp = document.getElementById('about-rwp').getBoundingClientRect();
+// var contact = document.getElementById('contact').getBoundingClientRect();
+// var careers = document.getElementById('careers').getBoundingClientRect();
+// var company = document.getElementById('company').getBoundingClientRect();
+//
+//
+// console.log(
+//   "welcome\nwelcome.y = " + welcome.y + "\nwelcome.top = " + welcome.top+ "\nwelcome.bottom = " + welcome.bottom )
+//   // "\naboutRwp\naboutRwp.y = " + aboutRwp.y + "\naboutRwp.top = " + aboutRwp.top+ "\naboutRwp.bottom = " + aboutRwp.bottom)
+//
+//
+// // Ternary Operator
+// // ( condition ) ? run this code : run this code instead
+//
+// let welcomeArea = (welcome.top < 0) ? welcome.bottom : height - welcome.bottom;
+// let servicesArea = (services.top < 0) ? services.bottom : height - services.bottom;
+// let aboutRwpArea = (aboutRwp.top < 0) ? aboutRwp.bottom : height - aboutRwp.bottom;
+// let contactArea = (contact.top < 0) ? contact.bottom : height - contact.bottom;
+// let careersArea = (careers.top < 0) ? careers.bottom : height - careers.bottom;
+// let companyArea = (company.top < 0) ? company.bottom : height - company.bottom;
+//
+//
+//
+// console.log("\nHeight = " + height);
+// console.log("\nwelcomeArea = " + welcomeArea);
+// console.log("\naboutRwpArea = " + aboutRwpArea);
+//
+// let areas = [welcomeArea, servicesArea, aboutRwpArea, contactArea, careersArea, companyArea];
+// let section = indexOfMax(areas);
+//
+// switch (section) {
+//   case 0: // welcome
+//     document.getElementById("snwelcome").classList.add('current');
+//     document.getElementById("snservices").classList.remove('current');
+//     break;
+//
+//   case 1: // services
+//     document.getElementById("snwelcome").classList.remove('current');
+//     document.getElementById("snservices").classList.add('current');
+//     document.getElementById("snabout-rwp").classList.remove('current');
+//     break;
+//
+//   case 2: // aboutRwp
+//     document.getElementById("snservices").classList.remove('current');
+//     document.getElementById("snabout-rwp").classList.add('current');
+//     document.getElementById("sncontact").classList.remove('current');
+//     break;
+//
+//   case 3: // contact
+//     document.getElementById("snabout-rwp").classList.remove('current');
+//     document.getElementById("sncontact").classList.add('current');
+//     document.getElementById("sncareers").classList.remove('current');
+//     break;
+//
+//   case 4: // careers
+//     document.getElementById("sncontact").classList.remove('current');
+//     document.getElementById("sncareers").classList.add('current');
+//     document.getElementById("sncompany").classList.remove('current');
+//     break;
+//
+//   case 5: // company
+//     document.getElementById("sncareers").classList.remove('current');
+//     document.getElementById("sncompany").classList.add('current');
+//     break;
+//
+//   }
+// }
