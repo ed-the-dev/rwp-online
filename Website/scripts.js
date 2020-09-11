a6489e0d7e9cf9f0a9e634ebd09182ef7feb2b1a
ed-walpole-on-pc <43918391+ed-walpole-on-pc@users.noreply.github.com>
2020-09-11 04:05:05 +0100

    0.1.15
    Animate on scroll written. Needs optimising.

--- Website/scripts.js
+++ Website/scripts.js
@@ -1,6 +1,6 @@
-var elements = document.getElementsByClassName("show-on-scroll");
+var sections = document.getElementsByClassName("content");
+var displayHeight = document.documentElement.clientHeight;
 
-// the html hasn't been read yet, so call this script after html?
 
 function scrolling() {
   var winScroll = document.getElementById("article").scrollTop;
@@ -67,31 +67,20 @@ function scrolling() {
 
   // A N I M A T E  O N  S C R O L L //
 
-  // console.log(elements);
-
-  for (var i = 0; i < elements.length; i++) {
-    console.log(elements.item(i));
+  for (var i = 0; i < sections.length; i++) {
+    if (isSectinVp(sections.item(i))) {
+      elements = sections.item(i).getElementsByClassName("show-on-scroll");
+      for (var j = 0; j < elements.length; j++) {
+        elements.item(j).classList.add('is-visible');
+      }
+    } else {
+      elements = sections.item(i).getElementsByClassName("show-on-scroll");
+      for (var j = 0; j < elements.length; j++) {
+        elements.item(j).classList.remove('is-visible');
+      }
+    }
   }
-
-  // check here that the element is being accessed.
-
-
-  // .length;
-
-  // elements.forEach(
-  //   function (element) {
-  //   if (isElinVp(element)) {
-  //     element.classList.add('is-visible');
-  //   } else {
-  //     element.classList.remove('is-visible');
-  //   }
-  // });
-
-
-
 }
-
-
 
 
 function openNav() {
@@ -125,86 +114,10 @@ function hideMap(){
   document.getElementById("withmap").classList.remove("show")
 }
 
-function isElinVp(element){
-
+function isSectinVp(section){
+  if(section.getBoundingClientRect().top > -1 && section.getBoundingClientRect().top <= displayHeight){
+    return true;
+  } else {
+    return false;
+  }
 }
-
-
-// window.onscroll = function() {progressIndicator()};
-
-// function progressIndicator() {
-// var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-// var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-// var displayHeight = document.documentElement.clientHeight;
-// var scrolled = (winScroll / height) * 100;
-// document.getElementById("myBar").style.width = scrolled + "%";
-//
-// var welcome = document.getElementById('welcome').getBoundingClientRect();
-// var services = document.getElementById('services').getBoundingClientRect();
-// var aboutRwp = document.getElementById('about-rwp').getBoundingClientRect();
-// var contact = document.getElementById('contact').getBoundingClientRect();
-// var careers = document.getElementById('careers').getBoundingClientRect();
-// var company = document.getElementById('company').getBoundingClientRect();
-//
-//
-// console.log(
-//   "welcome\nwelcome.y = " + welcome.y + "\nwelcome.top = " + welcome.top+ "\nwelcome.bottom = " + welcome.bottom )
-//   // "\naboutRwp\naboutRwp.y = " + aboutRwp.y + "\naboutRwp.top = " + aboutRwp.top+ "\naboutRwp.bottom = " + aboutRwp.bottom)
-//
-//
-// // Ternary Operator
-// // ( condition ) ? run this code : run this code instead
-//
-// let welcomeArea = (welcome.top < 0) ? welcome.bottom : height - welcome.bottom;
-// let servicesArea = (services.top < 0) ? services.bottom : height - services.bottom;
-// let aboutRwpArea = (aboutRwp.top < 0) ? aboutRwp.bottom : height - aboutRwp.bottom;
-// let contactArea = (contact.top < 0) ? contact.bottom : height - contact.bottom;
-// let careersArea = (careers.top < 0) ? careers.bottom : height - careers.bottom;
-// let companyArea = (company.top < 0) ? company.bottom : height - company.bottom;
-//
-//
-//
-// console.log("\nHeight = " + height);
-// console.log("\nwelcomeArea = " + welcomeArea);
-// console.log("\naboutRwpArea = " + aboutRwpArea);
-//
-// let areas = [welcomeArea, servicesArea, aboutRwpArea, contactArea, careersArea, companyArea];
-// let section = indexOfMax(areas);
-//
-// switch (section) {
-//   case 0: // welcome
-//     document.getElementById("snwelcome").classList.add('current');
-//     document.getElementById("snservices").classList.remove('current');
-//     break;
-//
-//   case 1: // services
-//     document.getElementById("snwelcome").classList.remove('current');
-//     document.getElementById("snservices").classList.add('current');
-//     document.getElementById("snabout-rwp").classList.remove('current');
-//     break;
-//
-//   case 2: // aboutRwp
-//     document.getElementById("snservices").classList.remove('current');
-//     document.getElementById("snabout-rwp").classList.add('current');
-//     document.getElementById("sncontact").classList.remove('current');
-//     break;
-//
-//   case 3: // contact
-//     document.getElementById("snabout-rwp").classList.remove('current');
-//     document.getElementById("sncontact").classList.add('current');
-//     document.getElementById("sncareers").classList.remove('current');
-//     break;
-//
-//   case 4: // careers
-//     document.getElementById("sncontact").classList.remove('current');
-//     document.getElementById("sncareers").classList.add('current');
-//     document.getElementById("sncompany").classList.remove('current');
-//     break;
-//
-//   case 5: // company
-//     document.getElementById("sncareers").classList.remove('current');
-//     document.getElementById("sncompany").classList.add('current');
-//     break;
-//
-//   }
-// }
